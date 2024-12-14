b3f20b39cbda20c9ce73f9e2b2e5babf9d4739d4
jei3m <virozzegt@gmail.com>
2024-12-14 18:30:02 +0800

    Replaced AI greeting to defined text instead of waiting for AI to generate a greeting

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -11,6 +11,7 @@ import { useApiKey } from "../context/ApiKeyContext";
 import ReactMarkdown from 'react-markdown';
 import { message } from 'antd';
 import { fetchSensorData, generateGreeting, generateAIResponse, fileToGenerativePart, getBase64 } from '../services/chatServices';
+import { MAX_TEMPERATURE } from "../services/sensorService"
 
 const AiwithImage = () => {
   const [image, setImage] = useState('');
@@ -122,8 +123,19 @@ const AiwithImage = () => {
       }
 
       try {
-        const greetingText = await generateGreeting(plantName, daysSincePlanting, temperature, humidity);
-        setMessages([{ user: false, text: greetingText }]);
+        const warningMessage = temperature > MAX_TEMPERATURE ? "**Warning:** Temperature is too Hot" : "Need help or have questions? Don&apos;t hesitate to ask!";
+
+        const greetingText = 
+`Hey there, I'm AI-Ponics, your friendly Aeroponic System Assistant! 👋 \n
+* Here's a quick update on your system:\n
+     *   **Plant:** ${plantName}
+     *   **Age:** ${daysSincePlanting}
+     *   **Temperature:** ${temperature}
+     *   **Humidity:** ${humidity}\n
+${warningMessage}`
+
+        // const greetingText = await generateGreeting(plantName, daysSincePlanting, temperature, humidity);
+        setMessages([{ user: false, text: greetingText}]);
       } catch (error) {
         console.error('Error generating greeting:', error);
         setMessages([{ user: false, text: "Sorry, I encountered an error while generating a greeting." }]);
