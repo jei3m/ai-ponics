379e6374f591edb60c0fd854cea6b2306faa6843
Sh10o <mjbeltran072202@gmail.com>
2025-01-23 11:20:46 +0800

    feat: updated live streaming

--- src/API.js
+++ src/API.js
@@ -2,17 +2,17 @@
 
 export const authToken = process.env.REACT_APP_VIDEOSDK_API_KEY;
 // API call to create a meeting
-export const createMeeting = async ({ token }) => {
+// API call to create stream
+export const createStream = async ({ token }) => {
   const res = await fetch(`https://api.videosdk.live/v2/rooms`, {
     method: "POST",
     headers: {
-      authorization: `${authToken}`,
+      authorization: `${token}`,
       "Content-Type": "application/json",
     },
     body: JSON.stringify({}),
   });
-  //Destructuring the roomId from the response
-  const { roomId } = await res.json();
-  return roomId;
+  //Destructuring the streamId from the response
+  const { roomId: streamId } = await res.json();
+  return streamId;
 };
-
