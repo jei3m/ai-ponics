67f2e42ce292ee6f250a40a5725a94c3e3111cfb
jei3m <virozzegt@gmail.com>
2025-03-15 22:35:31 +0800

    feat: isNearMax function to change color

--- src/pages/components/Sensors/Gauge.jsx
+++ src/pages/components/Sensors/Gauge.jsx
@@ -3,6 +3,7 @@ import { CircularProgress, Box, Typography } from '@mui/material';
 
 export function Gauge({ value, max, label }) {
     const normalizedValue = (value / max) * 100;
+    const isNearMax = normalizedValue >= 90; // Check if value is at 90% or more of max
 
     return (
         <Box position="relative" display="inline-flex">
@@ -25,7 +26,7 @@ export function Gauge({ value, max, label }) {
                     position: 'absolute',
                     top: 0,
                     left: 0,
-                    color: '#3cb371', 
+                    color: isNearMax ? '#e70000' : '#3cb371', // Red if near max, green otherwise
                     borderRadius: '50%',
                 }}
                 aria-label={`Progress: ${Math.round(value)}${label}`} // Accessible name for the progress
@@ -40,10 +41,16 @@ export function Gauge({ value, max, label }) {
                 alignItems="center"
                 justifyContent="center"
             >
-                <Typography variant="h5" component="div" color="textPrimary" sx={{
-                    fontFamily: 'Inter, sans-serif', fontWeight: '500'
-                }}>
-                    {`${Math.round(value)}${label}`}
+                <Typography 
+                    variant="h5" 
+                    component="div" 
+                    color={isNearMax ? 'error' : 'textPrimary'} 
+                    sx={{
+                        fontFamily: 'Inter, sans-serif', 
+                        fontWeight: '500'
+                    }}
+                >
+                    {`${value}${label}`}
                 </Typography>
             </Box>
         </Box>
