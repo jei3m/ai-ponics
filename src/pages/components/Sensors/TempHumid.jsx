e8ef08ab8fbc71a302bc2c526dbb22af74e1cba2
jei3m <virozzegt@gmail.com>
2025-03-17 22:16:33 +0800

    feat: status for temperature and humidity thresholds

--- src/pages/components/Sensors/TempHumid.jsx
+++ src/pages/components/Sensors/TempHumid.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Card, Flex } from 'antd';
+import { Card, Flex, Typography } from 'antd';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faThermometerHalf, faTint } from '@fortawesome/free-solid-svg-icons';
 import Gauge from './Gauge';
-import { StatusMessage } from "../../../services/sensorService";
+import { StatusMessage, MAX_TEMPERATURE, MIN_TEMPERATURE, MAX_HUMIDITY, MIN_HUMIDITY } from "../../../services/sensorService";
 import './css/TempHumid.css'
 
 function TempHumidCard({ temperature, humidity, status }) {
@@ -28,7 +28,20 @@ function TempHumidCard({ temperature, humidity, status }) {
               <StatusMessage message={status.message} className={status.className} style={status.style} />
             )}
             {!status && temperature !== null && (
-              <Gauge value={temperature} max={100} label="°C" />
+              <div>
+                <Gauge value={temperature} max={MAX_TEMPERATURE} min={MIN_TEMPERATURE} label="°C" />
+                <div>
+                  <Typography.Text strong className="temphumid-status">
+                      {temperature >= MIN_TEMPERATURE && temperature <= MAX_TEMPERATURE
+                          ? 'Normal'
+                          : temperature < MIN_TEMPERATURE
+                          ? 'Too Low'
+                          : temperature >= MAX_TEMPERATURE
+                          ? 'Too High'
+                          : ''}
+                  </Typography.Text>
+                </div>
+              </div>
             )}
           </div>
         </Card>
@@ -48,8 +61,22 @@ function TempHumidCard({ temperature, humidity, status }) {
               <StatusMessage message={status.message} className={status.className} style={status.style} />
             )}
             {!status && humidity !== null && (
-              <Gauge value={humidity} max={100} label="%" />
+              <div>
+                <Gauge value={humidity} max={MAX_HUMIDITY} min={MIN_HUMIDITY} label="%" />
+                <div>
+                  <Typography.Text strong className="temphumid-status">
+                      {humidity >= MIN_HUMIDITY && humidity <= MAX_HUMIDITY
+                          ? 'Normal'
+                          : humidity < MIN_HUMIDITY
+                          ? 'Too Low'
+                          : humidity > MAX_HUMIDITY
+                          ? 'Too High'
+                          : ''}
+                  </Typography.Text>
+                </div>
+              </div>
             )}
+
           </div>
         </Card>
         
