cb4b91896579c3688c8b2edcd4c5fdba1b156edb
jei3m <virozzegt@gmail.com>
2025-03-24 10:55:54 +0800

    ref: simplified lat and lon extraction from geoapify api

--- src/pages/components/Sensors/WeatherCard.jsx
+++ src/pages/components/Sensors/WeatherCard.jsx
@@ -173,7 +173,7 @@ function WeatherCard() {
       const barangayName = selectedBarangayObj ? selectedBarangayObj.brgy_name : '';
       
       const addressQuery = encodeURIComponent(
-        `${barangayName} ${cityName}, ${provinceName}, Philippines`
+        `${barangayName}, ${cityName}, ${provinceName}, Philippines`
       );
 
       // Get coordinates using Geoapify
@@ -181,11 +181,11 @@ function WeatherCard() {
         `https://api.geoapify.com/v1/geocode/search?text=${addressQuery}&apiKey=${process.env.REACT_APP_GEOAPIFY_API_KEY}`
       );
       const geoData = await geoRes.json();
-
+      
       if (geoData.features && geoData.features.length > 0) {
-        const coordinates = geoData.features[0].geometry.coordinates;
-        const [lon, lat] = coordinates;
-        
+        const lon = geoData.features[0].properties.lon;
+        const lat = geoData.features[0].properties.lat;
+        console.log(lon, lat);
         // Get weather data using coordinates
         const weatherRes = await fetch(
           `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`
